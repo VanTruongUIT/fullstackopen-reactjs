65132938ba2bf429e50e6a60181208b93e6ac67f
VanTruongUIT <tvtruong.uit@gmail.com>
2022-07-10 13:38:49 +0700

    complete unicafe step 2

--- src/App.js
+++ src/App.js
@@ -10,31 +10,59 @@ const Button = (props) => {
   );
 };
 
+
+
 const App = () => {
 
   // save clicks of each button its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const [allFeedBack, setAllFeedBack] = useState(0);
+  const [numberOfFeedBacks, setNumberOfFeedBacks] = useState(0);
+  const [totalPoint, setTotalPoint] = useState(0);
+  const [average, setAverage] = useState(0);
+  const [positive, setPositive] = useState(0);
 
-  const sumFeedBack = () => {
-    return good + neutral + bad;
-  }
+  const calculateAverage = () => {
+    console.log('totalpoint', totalPoint);
+    console.log('numberOfFeedBacks', numberOfFeedBacks);
+    let average = totalPoint / numberOfFeedBacks;
+    console.log('average', average);
+    return average;
+  };
+
+  const calculatePositive = () => { 
+    return (good + neutral) / numberOfFeedBacks;
+  };
 
   const handleSetGoodClick = () => { 
     setGood(good + 1);
-    console.log('call set All FeedBack');
-    setAllFeedBack(sumFeedBack());
+    console.log('good', good);
+
+    setTotalPoint(totalPoint + 1);
+    console.log('totalPoint', totalPoint);
+
+    setNumberOfFeedBacks(numberOfFeedBacks + 1);
+    console.log('numberOfFeedBacks', numberOfFeedBacks);
+    console.log('average', (good - bad) / numberOfFeedBacks);
+    setAverage((good - bad) / numberOfFeedBacks);
+    setPositive(calculatePositive());
 
   }
   const handleSetNeutralClick = () => { 
     setNeutral(neutral + 1);
-    setAllFeedBack(sumFeedBack());
+    setNumberOfFeedBacks(numberOfFeedBacks + 1);
+    setAverage(calculateAverage());
+    setPositive(calculatePositive());
+
   }
   const handleSetBadClick = () => { 
     setBad(bad + 1);
-    setAllFeedBack(sumFeedBack());
+    setTotalPoint(totalPoint - 1);
+    setNumberOfFeedBacks(numberOfFeedBacks + 1);
+    setAverage(calculateAverage());
+    setPositive(calculatePositive());
+
   }
 
   return (
@@ -64,8 +92,15 @@ const App = () => {
         Bad {bad}
       </div>
       <div>
-        All {allFeedBack}
+        All {numberOfFeedBacks}
       </div>
+      <div>
+        Average {average}
+      </div>
+      <div>
+        Positive {positive}
+      </div>
+
     </div>
   );
 };
